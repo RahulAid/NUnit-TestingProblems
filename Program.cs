24b6b65923d04947a8b7c634f9c473b418d285e7
RahulAid <raccess97@gmail.com>
2022-10-31 04:31:35 +0530

    Program to Print Day of the Week

--- Program.cs
+++ Program.cs
@@ -3,34 +3,49 @@
     internal class Program
     {       public static void Main()
          {
-            Console.WriteLine("Choose the unit conversion to be performed : \n1.Celsius to Fahrenheit \n2.Fahrenheit-Celsius");
-            int In = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Day value: ");
+            int d = Convert.ToInt32(Console.ReadLine());
 
-            if (In == 1)
+            Console.WriteLine("\nEnter Month value: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nEnter Year value: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            int y0 = y - (14 - m) / 12;
+            int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
+            int m0 = m + 12 * ((14 - m) / 12) - 2;
+            int d0 = (d + x + (31 * m0) / 12) % 7;
+
+            switch (d0)
             {
-                Console.WriteLine("\nEnter a temperature value: ");
-                double val1 = Convert.ToInt32(Console.ReadLine());
-
-                double result1 = (val1 * 9 / 5) + 32;
-
-                Console.WriteLine("\n{0} °C equals {1} °F", val1, result1);
-                Console.ReadLine();
+                case 0:
+                    Console.WriteLine("\nIt is SUNDAY");
+                    break;
+                case 1:
+                    Console.WriteLine("\nIt is MONDAY");
+                    break;
+                case 2:
+                    Console.WriteLine("\nIt is TUESDAY");
+                    break;
+                case 3:
+                    Console.WriteLine("\nIt is WEDNESDAY");
+                    break;
+                case 4:
+                    Console.WriteLine("\nIt is THURSDAY");
+                    break;
+                case 5:
+                    Console.WriteLine("\nIt is FRIDAY");
+                    break;
+                case 6:
+                    Console.WriteLine("\nIt is SATURDAY");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
             }
-            else if (In == 2)
-            {
-                Console.WriteLine("\nEnter a temperature value: ");
-                double val2 = Convert.ToInt32(Console.ReadLine());
-
-                double result2 = (val2 - 32) * 5 / 9;
-
-                Console.WriteLine("\n{0} °F equals {1} °C", val2, result2);
-                Console.ReadLine();
-            }
-            else
-            {
-                Console.WriteLine("\nEnter a Valid Temperature Value");
-                Console.ReadLine();
-            }
+            Console.ReadLine();
         }
+        
     }
 }
