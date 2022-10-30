3389e40fe1c3468a47fa34ee9027d303272cbe90
RahulAid <raccess97@gmail.com>
2022-10-31 04:21:37 +0530

    Temperature Unit Conversion

--- Program.cs
+++ Program.cs
@@ -1,17 +1,36 @@
 ﻿namespace NUnit_TestingProblems
 {
     internal class Program
-    {
-        static int swapNibbles(int x)
-        {
-            return ((x & 0x0F) << 4 | (x & 0xF0) >> 4);
-        }
+    {       public static void Main()
+         {
+            Console.WriteLine("Choose the unit conversion to be performed : \n1.Celsius to Fahrenheit \n2.Fahrenheit-Celsius");
+            int In = Convert.ToInt32(Console.ReadLine());
 
-        public static void Main()
-        {
-            Console.WriteLine("Enter a Value to Swap: ");
-            int n = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("\nSwapped Number is: {0}", swapNibbles(n));
+            if (In == 1)
+            {
+                Console.WriteLine("\nEnter a temperature value: ");
+                double val1 = Convert.ToInt32(Console.ReadLine());
+
+                double result1 = (val1 * 9 / 5) + 32;
+
+                Console.WriteLine("\n{0} °C equals {1} °F", val1, result1);
+                Console.ReadLine();
+            }
+            else if (In == 2)
+            {
+                Console.WriteLine("\nEnter a temperature value: ");
+                double val2 = Convert.ToInt32(Console.ReadLine());
+
+                double result2 = (val2 - 32) * 5 / 9;
+
+                Console.WriteLine("\n{0} °F equals {1} °C", val2, result2);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("\nEnter a Valid Temperature Value");
+                Console.ReadLine();
+            }
         }
     }
 }
