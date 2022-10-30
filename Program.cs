d8f6a2e4c2f88e50370eaf33041902399eaa294a
RahulAid <raccess97@gmail.com>
2022-10-31 04:04:35 +0530

    Program to Calculate Square Root

--- Program.cs
+++ Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("NUnit-TestingProblems");
+            Console.WriteLine("Enter a value: ");
+            double n = Convert.ToInt32(Console.ReadLine());
+
+            double SquareRoot = Math.Sqrt(n);
+            Console.WriteLine("\nThe Square Root of {0} is : {1}", n, SquareRoot);
         }
     }
 }
